e6c752503edf2bdbef1778fe2a258ff67148add3
Xavionstar <lincpough@gmail.com>
2023-04-20 17:51:58 -0700

    basic schema for mongoose models implemented

--- src/models/index.js
+++ src/models/index.js
@@ -1,4 +1,9 @@
 const User = require('./users');
 const Blog = require('./blogs');
+const Security = require('./securities');
+const Shop = require('./shops');
+const Social = require('./socials');
 
-module.exports = {User, Blog};
+
+
+module.exports = {User, Blog, Security, Shop, Social};
--- src/models/socials.js
+++ src/models/socials.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const opts = { timestamps: true, toJSON: { virtuals: true } };
-const shopReviewSchema = mongoose.Schema({
+const socialReviewSchema = mongoose.Schema({
     review: { type: String, required: true , minLength: 5, maxLength: 280},
     user: {type: String, required: true, ref: 'User'}
 });
@@ -9,7 +9,8 @@ const creatorSchema = mongoose.Schema({
     email: { type: String, required: true, unique: true, match: [/.+@.+\..+/, 'Must match an email address!'] },
 
 });
-const shopSchema = mongoose.Schema({
+
+const socialSchema = mongoose.Schema({
     projectTitle: {
         type: String,
         required: true,
@@ -23,19 +24,19 @@ const shopSchema = mongoose.Schema({
         type: Number,
         required: true
     },
-    reviews:  [shopReviewSchema],
+    reviews:  [socialReviewSchema],
     rating: {
         type: Number,
         required: true
     },
     creator : [creatorSchema]
-},
+}, 
 opts
 );
 
-shopReviewSchema.virtual('reviewCount').get(function() {
+socialReviewSchema.virtual('reviewCount').get(function() {
     return this.reviews.length;
   });
 
-const Shop = mongoose.model('Shop', shopSchema);
-module.exports = Shop;
+const Social = mongoose.model('Security', socialSchema);
+module.exports = Social;
